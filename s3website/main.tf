b05e2754afaa3d21dd2698da6d8c1c741fcd5fab
Andrew Griffiths <mail@andrewgriffithsonline.com>
2018-01-28 21:19:46 +0000

    Adds s3 website bucket policy

--- s3website/main.tf
+++ s3website/main.tf
@@ -36,7 +36,8 @@ data "aws_iam_policy_document" "s3_website_policy_root" {
 
 resource "aws_s3_bucket" "main" {
   bucket = "${var.domain}"
-
+   acl = "public-read"
+policy = "${data.aws_iam_policy_document.s3_website_policy_root.json}"
   website {
     index_document = "index.html"
     error_document = "404.html"
