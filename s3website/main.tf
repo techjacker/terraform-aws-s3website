937160aa4e9c3d2aa49cfb40778927075e82345e
Andrew Griffiths <mail@andrewgriffithsonline.com>
2018-02-10 16:35:39 +0000

    404 returns 404.html

--- s3website/main.tf
+++ s3website/main.tf
@@ -106,7 +106,7 @@ resource "aws_cloudfront_distribution" "website" {
   custom_error_response {
     error_code         = 404
     response_code      = 404
-    response_page_path = "/404"
+    response_page_path = "/404.html"
   }
 
   default_cache_behavior {
