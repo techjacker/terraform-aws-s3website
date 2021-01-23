a484c7fcfc64db49f288618d830af5a8e9d644f8
Andrew Griffiths <mail@andrewgriffithsonline.com>
2021-01-23 20:39:50 +0000

    Fixes duplicate text record error

--- mx/main.tf
+++ mx/main.tf
@@ -1,24 +1,21 @@
-# locals {
-#   records = [1,2,3]
-# }
+resource "aws_route53_record" "mx_txt" {
+  zone_id = var.zone_id
+  name    = var.domain
+  type    = "TXT"
+  ttl     = "300"
+  records = [var.mx_txt, var.mx_spf]
+}
 
 resource "aws_route53_record" "mx_dkim_arc" {
   count   = 3
   zone_id = var.zone_id
-  # for_each = local.records
-  # for_each = {
-  #   for record in local.records :
-  # }
-  # name    = "key${each.value}._domainkey"
-  name = "key${count.index + 1}._domainkey"
-  type = "CNAME"
-  ttl  = "300"
+  name    = "key${count.index + 1}._domainkey"
+  type    = "CNAME"
+  ttl     = "300"
   records = [
-    # "key${each.value}.commonplacepurpose.com._domainkey.migadu.com."
     "key${count.index + 1}.commonplacepurpose.com._domainkey.migadu.com."
   ]
 }
-
 
 
 
@@ -28,30 +25,6 @@ resource "aws_route53_record" "mx" {
   type    = "MX"
   ttl     = "300"
   records = var.mx
-}
-
-resource "aws_route53_record" "mx_spf" {
-  zone_id = var.zone_id
-  name    = var.domain
-  type    = "TXT"
-  ttl     = "300"
-  records = [var.mx_spf]
-}
-
-resource "aws_route53_record" "mx_spf_spf" {
-  zone_id = var.zone_id
-  name    = var.domain
-  type    = "SPF"
-  ttl     = "300"
-  records = [var.mx_spf]
-}
-
-resource "aws_route53_record" "mx_txt" {
-  zone_id = var.zone_id
-  name    = var.domain
-  type    = "TXT"
-  ttl     = "300"
-  records = [var.mx_txt]
 }
 
 resource "aws_route53_record" "mx_dmarc" {
